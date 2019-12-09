4afbb0c888e22d624c8aa785e7f6a1290d6e651b
Aiman <aimanaaw@gmail.com>
2019-12-09 14:29:45 -0500

    Creating attributes

--- something.py
+++ something.py
@@ -1,5 +1,15 @@
-class Sample():
-  pass
+class Dog():
+  def __init__(self, mybreed, name, spots):
 
-my_sample = Sample();
-print(type(my_sample))
+    self.mybreed = mybreed
+    self.name = name
+    # Expect a boolean
+    self.spots = spots
+
+
+my_dog = Dog('lab', 'sammy', False)
+
+print(type(my_dog))
+print('dog name is', my_dog.name)
+
+print('dog has spots? ', my_dog.spots)
