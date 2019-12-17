889a7058b0d9d33d2fe4fc20f62899d15a80c804
Aiman <aimanaaw@gmail.com>
2019-12-16 20:12:32 -0500

    Modules and packages

---
+++ myMainPackage/subPackage/my_sub_script.py
@@ -0,0 +1,2 @@
+def sub_report():
+  print ("HEY i am a function inside my subscript")
---
+++ myprogram.py
@@ -0,0 +1,13 @@
+# import from module
+# from mymodule import my_func
+
+# my_func()
+
+# to import from package
+from myMainPackage import some_main_script
+from myMainPackage.subPackage import my_sub_script
+
+some_main_script.report_main()
+
+my_sub_script.sub_report()
+
