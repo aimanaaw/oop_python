b12ff45b9df4737a1f0ef747e693b6bf93c39cea
Aiman <aimanaaw@gmail.com>
2020-01-03 02:34:14 -0500

    Decorator @function

--- decorators_callback.py
+++ decorators_callback.py
@@ -6,10 +6,11 @@ def new_decorator(original_func):
     print('Some extra code here, after the original function!')
   return wrap_func
 
+@new_decorator
 def func_needs_decorator():
   print("I want to be decorated!")
 
 func_needs_decorator()
 
-decorated_func = new_decorator(func_needs_decorator)
-decorated_func()
+# decorated_func = new_decorator(func_needs_decorator)
+# decorated_func()
