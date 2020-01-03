d32366012cf0fba310271551312826adc1c8b003
Aiman <aimanaaw@gmail.com>
2020-01-03 02:18:00 -0500

    Callback func in python

--- decorators_callback.py
+++ decorators_callback.py
@@ -1,8 +1,15 @@
-def hello():
-  return 'Hi  Jose!'
+def new_decorator(original_func):
 
-def other(some_def_func):
-  print('Other code runs here!')
-  print(some_def_func())
+  def wrap_func():
+    print('Some extra code here, before the original function!')
+    original_func()
+    print('Some extra code here, after the original function!')
+  return wrap_func
 
-other(hello)
+def func_needs_decorator():
+  print("I want to be decorated!")
+
+func_needs_decorator()
+
+decorated_func = new_decorator(func_needs_decorator)
+decorated_func()
