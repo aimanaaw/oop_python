b5cbdb6223a50dbac360443fb1b97450742c54a8
Aiman <aimanaaw@gmail.com>
2020-01-03 15:40:13 -0500

    Next generator

--- generators.py
+++ generators.py
@@ -1,9 +1,11 @@
-def gen_fibon(n):
-  a = 1
-  b = 1
-  for i in range(n):
-    yield a
-    a,b = b, a+b
+def simple_gen():
+  for x in range(3):
+    yield x
 
-for number in gen_fibon(10):
+for number in simple_gen():
   print(number)
+
+g = simple_gen()
+print(next(g))
+print(next(g))
+print(next(g))
