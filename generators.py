ef64e66fdef8582f95da428c652bf2dd46ca8c1a
Aiman <aimanaaw@gmail.com>
2020-01-03 12:45:35 -0500

    Generating fibonaccy

--- generators.py
+++ generators.py
@@ -1,6 +1,9 @@
-def create_cubes(n):
-  for x in range(n):
-    yield x**3
+def gen_fibon(n):
+  a = 1
+  b = 1
+  for i in range(n):
+    yield a
+    a,b = b, a+b
 
-for x in create_cubes(10):
-  print(x)
+for number in gen_fibon(10):
+  print(number)
