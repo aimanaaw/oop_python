5029c5ec7edef594657187f1b93433a888087505
Aiman <aimanaaw@gmail.com>
2020-01-03 01:51:53 -0500

    Function scopes

--- decorators.py
+++ decorators.py
@@ -1,4 +1,17 @@
 def func():
   return 1
 
-func()
+def hello(name='Jose'):
+  print('The hello() function has been executed')
+
+  def greet():
+    return '\t This is the greet() function inside hello!'
+
+  def welcome():
+    return '\t This is welcome() inside hello()'
+  print(greet())
+  print(welcome())
+  print('This is the end of the hello function')
+
+hello()
+
